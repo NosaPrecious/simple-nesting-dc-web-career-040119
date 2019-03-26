6346006fbcea25ff4d669a5a1789cfc58a788a55
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-26 19:24:16 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,5 @@
 
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -16,7 +17,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,8 +82,23 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+  programmer_hash[:yukihiro_matsumoto]= {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  
+  programmer_hash
+  
+   # skills= {:known_for => "Ruby", :languages => ["LISP", "C"]}
+    # n_arr = programmer_hash.to_a
+    # n_arr.unshift([:yukihiro_matsumoto,{}])
+    # n_hash= n_arr.to_h
+   #  n_hash[:yukihiro_matsumoto] = skills
+    # n_hash[:yukihiro_matsumoto]
     
+  #binding.pry
+
 end
 
 def changing_alan
@@ -101,8 +119,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for]= alans_new_info
+    return  programmer_hash
      
-     
+     #binding.pry
 end
 
 def adding_to_dennis
@@ -121,5 +141,11 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     
+    n_arr= programmer_hash[:dennis_ritchie][:languages]
+    
+    n_arr.push("Assembly")
+    #binding.pry
+    
+    programmer_hash
 end
